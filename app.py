17f263623e8c22056464706bf6adec591ac1b583
suvan362 <40496225+suvan362@users.noreply.github.com>
2025-01-26 02:32:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,6 @@ def file_details():
 
 
 if __name__ == '__main__':
-    if not os.path.exists('uploads'):
-        os.makedirs('uploads')
+    if not os.path.exists("./uploads"):
+        os.makedirs("./uploads")
     app.run(debug=True)
